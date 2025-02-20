4e420c8c2bdad5f2caafaccd48fbe6ac9a7cf131
Yecheng Zhou <zhouych87@gmail.com>
2025-02-20 18:38:08 +0800

    merge code

--- logPpredict.py
+++ logPpredict.py
@@ -7,13 +7,15 @@ import libset
 import pickle
 from pathlib import Path
 from io import StringIO
+from sklearn.metrics import r2_score,mean_absolute_error
 import sys
 
 ndecimal=0
 sllist=[0.5]
 nsc=500
 
-fname = 'SAMPL6_molecules.csv' #'SAMPL9_molecules.csv'
+fname = sys.argv[1] #'SAMPL6_molecules.csv' #'SAMPL9_molecules.csv'
+
 dat=pd.read_csv(fname)
 length=len(dat) #488good,552
 y=dat.log_P
@@ -50,6 +52,30 @@ for i in range(length):
             print(i," processing error")
             print("This data is dropped")
 
-name='SAMPL6_s50.pkl' #'SAMPL9_s50.pkl'
-with open(name,'wb') as f:
-    pickle.dump(my, f)
+x = my[:,1:-1]
+y = my[:,-1]
+
+threshold = 5e-2
+fea_path = 'fea.pkl'
+model_path = 'model.pkl'
+fea_imp = pickle.load(open('fea.pkl','rb'))
+reg = pickle.load(open('model.pkl','rb'))
+
+selected_features = fea_imp > threshold
+
+selected_x = x[:,selected_features]
+y_predicted = reg.predict(selected_x)
+
+
+np.set_printoptions(precision=3,  suppress=False)
+print('Predicted log P:')
+print(y_predicted)
+
+r2 = r2_score(y, y_predicted)
+mae = mean_absolute_error(y, y_predicted)
+rmse = np.sqrt(mse)
+
+print('Predict: MAE=%.4f , RMSE=%.4f , R2=%.4f'%(mae_SAMPL6,rmse_SAMPL6,r2_SAMPL6))
+
+
+
